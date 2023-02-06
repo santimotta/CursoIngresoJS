1f3d5045868b67844692d06ce0fd4f670a554c51
santimotta <santiagomotta.sm.sm@gmail.com>
2023-02-06 14:58:18 -0300

    apuntes de if y switch

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -2,6 +2,13 @@
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
+
+
+
+TP2
+Santiago Motta
+
+
 */
 
 
@@ -56,15 +63,12 @@ function Rectangulo ()
 function Circulo () 
 {
     //Definimos variables
-    let ancho;
-    let largo;
     let radio;
     let diametroCirc;
     let alamCirc;
 
     //Asignamos valor a las variables
-    ancho = parseInt(document.getElementById("txtIdAncho").value);
-    largo = parseInt(document.getElementById("txtIdLargo").value);
+
     radio = parseInt(document.getElementById("txtIdRadio").value);
 
     //La circunferencia se calcula como PI = circunferencia/diametro = PI*diamtro = circunferencia = 2radio * PI = circunferencia
